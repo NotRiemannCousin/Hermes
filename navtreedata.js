a1e5822d354ae0e84cf6b9bcd0ee721694deb3a5
NotRiemannCousin <NotRiemannCousin@users.noreply.github.com>
2026-06-14 22:55:19 +0000

    deploy: dc426959091d1efce6f34606b85fb6ca39fc5c97

--- navtreedata.js
+++ navtreedata.js
@@ -25,16 +25,16 @@
 var NAVTREE =
 [
   [ "Hermes", "index.html", [
-    [ "</blockquote>", "index.html#autotoc_md21", null ],
-    [ "Features", "index.html#autotoc_md22", null ],
-    [ "Installation", "index.html#autotoc_md24", [
-      [ "pixi", "index.html#autotoc_md25", null ],
-      [ "vcpkg", "index.html#autotoc_md26", null ],
-      [ "Manual (CMake)", "index.html#autotoc_md27", null ]
+    [ "</blockquote>", "index.html#autotoc_md1", null ],
+    [ "Features", "index.html#autotoc_md2", null ],
+    [ "Installation", "index.html#autotoc_md4", [
+      [ "pixi", "index.html#autotoc_md5", null ],
+      [ "vcpkg", "index.html#autotoc_md6", null ],
+      [ "Manual (CMake)", "index.html#autotoc_md7", null ]
     ] ],
-    [ "Example", "index.html#autotoc_md29", null ],
-    [ "Roadmap", "index.html#autotoc_md31", null ],
-    [ "Requirements", "index.html#autotoc_md33", null ],
+    [ "Example", "index.html#autotoc_md9", null ],
+    [ "Roadmap", "index.html#autotoc_md11", null ],
+    [ "Requirements", "index.html#autotoc_md13", null ],
     [ "Topics", "topics.html", "topics" ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
